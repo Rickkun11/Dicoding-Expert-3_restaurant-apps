11bca32a074bc172e39d428a1b0976988290a743
rickkun11 <rickkun11@gmail.com>
2022-05-13 17:00:20 +0700

    navbar hamburger

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,8 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import ('../styles/main.css');
+import ('../scripts/bar.js')
 
+
+function toggleMobileMenu(menu) {
+    menu.classList.toggle('open');
+}
