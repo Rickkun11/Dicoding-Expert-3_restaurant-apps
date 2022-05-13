8d531323ce8764e5edb2c1e62e219faf611e57a1
rickkun11 <rickkun11@gmail.com>
2022-05-13 17:01:21 +0700

    delete import

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,2 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import ('../styles/main.css');
-import ('../scripts/bar.js')
-
-
-function toggleMobileMenu(menu) {
-    menu.classList.toggle('open');
-}
