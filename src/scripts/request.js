8704da1962fc81fda0086d6e46bff6558f3b09c0
rickkun11 <rickkun11@gmail.com>
2022-05-15 12:55:17 +0700

    final

--- src/scripts/request.js
+++ src/scripts/request.js
@@ -1,4 +1,5 @@
 import data from '../DATA.json';
+import warung from '../DATA2.json';
 
 data.restaurants.forEach((data) => {
 
@@ -31,6 +32,35 @@ data.restaurants.forEach((data) => {
         <p class="card-text">${data.rating}</p>
         <p class="card-text">${data.name}</p>
         <p class="card-text">${data.description}</p>
+    </div>
+    `;
+    console.log(data);
+});
+
+
+warung.warung.forEach((data) => {
+
+    document.getElementById("featur2").innerHTML = `
+    <div class="feature-container">
+            <p class="card-text">${data.id}</p>
+            <h2>${data.title}</h2>
+            <p class="card-text">${data.time}</p>
+            <p class="card-text">${data.servings}</p>
+            <p class="card-text">${data.difficulty}</p>
+    </div>
+    <div class="feature-container">
+            <p class="card-text">${data.id}</p>
+            <h2>${data.title}</h2>
+            <p class="card-text">${data.time}</p>
+            <p class="card-text">${data.servings}</p>
+            <p class="card-text">${data.difficulty}</p>
+    </div>
+    <div class="feature-container">
+            <p class="card-text">${data.id}</p>
+            <h2>${data.title}</h2>
+            <p class="card-text">${data.time}</p>
+            <p class="card-text">${data.servings}</p>
+            <p class="card-text">${data.difficulty}</p>
     </div>
     `;
     console.log(data);
