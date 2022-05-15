06025d4bb7d6f1020caabc288143286b82084c30
rickkun11 <rickkun11@gmail.com>
2022-05-15 14:10:40 +0700

    revisi

--- src/scripts/request.js
+++ src/scripts/request.js
@@ -25,14 +25,6 @@ data.restaurants.forEach((data) => {
         <p class="card-text">${data.name}</p>
         <p class="card-text">${data.description}</p>
     </div>
-    
-    <div class="feature-container">
-        <img src="${data.pictureId}" alt="Flexbox Feature">
-        <h2>${data.city}</h2>
-        <p class="card-text">${data.rating}</p>
-        <p class="card-text">${data.name}</p>
-        <p class="card-text">${data.description}</p>
-    </div>
     `;
     console.log(data);
 });
